a5721bea25d69f0ca8e48f4bfee473658ef3a0dc
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-04-07 16:42:45 +0300

    feature: implement entry point

---
+++ src/js/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <main>
+                <h1>Привет, Реакт!</h1>
+            </main>
+        );
+    }
+}
+
+export default App;
