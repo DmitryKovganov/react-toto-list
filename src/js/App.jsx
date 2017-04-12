a6f8a55af49e28c0cdd3eb65a1a0b7d6c9d65451
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-04-12 10:52:32 +0300

    feature: implement basic logic

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -5,10 +5,59 @@ import Header from './Components/Header';
 class App extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             todos: this.props.initialData
         };
+
+        this._nextId = this.state.todos.length;
+
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
+
+    nextId() {
+        return this._nextId += 1;
+    }
+
+    handleAdd(title) {
+        const todo = {
+            id: nextId(),
+            title: title,
+            completed: false
+        };
+
+        const todos = [...this.state.todos, todo];
+        this.setState({ todos });
+    }
+
+    handleDelete(id) {
+        let todos = this.state.todos;
+        const index = todos.findIndex(todo => todo.id === id);
+        const newTodos = todos.slice(0, index).concat(todos.slice(index + 1));
+        this.setState({ newTodos });
+    }
+
+    handleToggle(id) {
+        const todos = this.state.todos.map(todo => {
+            if (todo.id !== id) return todo;
+            return {}.assign({}, todo, { completed: !todo.completed });
+        });
+
+        this.setState({ todos });
+    }
+
+    handleEdit(id, title) {
+        const todos = this.state.todos.map(todo => {
+            if (todo.id !== id) return todo;
+            return {}.assign({}, todo, { title: title });
+        });
+
+        this.setState({ todos });
+    }
+
 
     render() {
         return (
