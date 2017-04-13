680f2715e44fb9ea8617fa349806d0a30b5be333
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-04-13 10:12:59 +0300

    implement: add redux

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,81 +1,17 @@
 import React from 'react';
 
-import Header from './Components/Header';
-import List from './Components/List';
-import Form from './Components/Form';
+import HeaderContainer from './containers/HeaderContainer';
+import ListContainer from './containers/ListContainer';
+import FormContainer from './containers/FormContainer';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            todos: this.props.initialData
-        };
-
-        this._nextId = this.state.todos.length;
-
-        this.handleAdd = this.handleAdd.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleToggle = this.handleToggle.bind(this);
-        this.handleEdit = this.handleEdit.bind(this);
-    }
-
-    nextId() {
-        return this._nextId += 1;
-    }
-
-    handleAdd(title) {
-        const todo = {
-            id: this.nextId(),
-            title: title,
-            completed: false
-        };
-
-        const todos = [...this.state.todos, todo];
-        this.setState({ todos });
-    }
-
-    handleDelete(id) {
-        let prevTodos = this.state.todos;
-        const index = prevTodos.findIndex(todo => todo.id === id);
-        const todos = prevTodos.slice(0, index).concat(prevTodos.slice(index + 1));
-        this.setState({ todos });
-    }
-
-    handleToggle(id) {
-        const todos = this.state.todos.map(todo => {
-            if (todo.id !== id) return todo;
-            return Object.assign({}, todo, { completed: !todo.completed });
-        });
-
-        this.setState({ todos });
-    }
-
-    handleEdit(id, title) {
-        const todos = this.state.todos.map(todo => {
-            if (todo.id !== id) return todo;
-            return Object.assign({}, todo, { title: title });
-        });
-
-        this.setState({ todos });
-    }
-
-
-    render() {
-        const todos = this.state.todos;
-        return (
-            <main>
-                <Header todos={todos} />
-                <List
-                    todos={todos}
-                    onEdit={this.handleEdit}
-                    onDelete={this.handleDelete}
-                    onToggle={this.handleToggle}
-                />
-                <Form onAdd={this.handleAdd} />
-            </main>
-        );
-    }
+function App() {
+    return (
+        <main>
+            <HeaderContainer/>
+            <ListContainer/>
+            <FormContainer/>
+        </main>
+    );
 }
 
 export default App;
