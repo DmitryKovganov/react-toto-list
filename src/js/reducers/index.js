a8b9b2c9e3a442992c2ac4337ad79a08b24ec93b
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-04-13 10:00:06 +0300

    feature: implement reducers

---
+++ src/js/reducers/index.js
@@ -0,0 +1,44 @@
+import {ADD_TODO, DELETE_TODO, EDIT_TODO, TOGGLE_TODO} from '../actions';
+
+function todoReducer(state = {}, action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return {
+                id: action.id,
+                title: action.title,
+                completed: false
+            };
+
+        case DELETE_TODO:
+            if (state.id === action.id) return false;
+            return true;
+
+        case EDIT_TODO:
+            if (state.id !== action.id) return todo;
+            return Object.assign({}, state, {title: action.title});
+
+        case TOGGLE_TODO:
+            if (state.id !== action.id) return todo;
+            return Object.assign({}, state, {completed: !action.completed});
+    }
+}
+
+function reducer(state = [], action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return [...state, todoReducer(undefined, action)];
+
+        case DELETE_TODO:
+            return state.todos.filter(todo => todoReducer(todo, action));
+
+        case EDIT_TODO:
+            return state.todos.map(todo => todoReducer(todo, action));
+
+        case TOGGLE_TODO:
+            return state.todos.map(todo => todoReducer(todo, action));
+        default:
+            return state;
+    }
+}
+
+export default reducer;
