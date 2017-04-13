62dd8941ad150bb522fd93852aa26f99fd2c43e2
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-04-13 13:04:16 +0300

    feature: change structure of store

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
-import reducer from './reducers';
-import state from './state';
 import App from './App';
-
-const store = createStore(reducer, state);
+import store from './store';
 
 ReactDOM.render(
     <Provider store={store}>
